e98daa06aa4a4aa133c359aba094a33f5726c2b9
jk87603 <38652811+jk87603@users.noreply.github.com>
2018-04-23 10:29:50 +0000

    Create player.c

---
+++ variable/player.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    char Name[30]="Sachin Ramesh Tendulkar";
+    char Nick_name[25]="Little Master";
+    int Runs=18246;
+    float Batting_average=44.83;
+    int matches=463;
+    return 0;
+}
