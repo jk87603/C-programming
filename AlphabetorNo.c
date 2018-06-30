c3fad1bb9d9b7e2a0e5d30d8fc19d10ba899a98b
jk87603 <38652811+jk87603@users.noreply.github.com>
2018-06-30 07:17:15 +0000

    Rename alphabetorNo.c to AlphabetorNo.c

---
+++ AlphabetorNo.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+int main()
+{
+  char input;
+  scanf("%c",&input);
+  if(input>=65 && input<=90 ||  input>=97 && input<=122)
+  {
+    printf("ALphabet");
+  }
+  else
+  {
+    printf("No");
+  }
+}
