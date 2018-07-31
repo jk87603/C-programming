84a2b40f62d27d5fa018f2053369fe3ff16b7720
jk87603 <38652811+jk87603@users.noreply.github.com>
2018-07-31 10:49:55 +0000

    Update large.c

--- beginner level/large.c
+++ beginner level/large.c
@@ -1,13 +1,12 @@
 #include<stdio.h>
-void main()
+int main()
 {
-	int a,b,c;
-	printf("Enter three numbers");
-	scanf("%d %d %d",&a,&b,&c);
-	if((a>b)&&(a>c))
-		printf("%d",a);
-	else if(b>c)
-		printf("%d",b);
-	else
-		printf("%d",c);
+int a,b,c;
+scanf("%d%d%d",&a,&b,&c);
+if((a>b)&&(a>c))
+printf("a is greater");
+else if((b>a)&&(b>c))
+printf("b is greater");
+else
+printf("c is greater");
 }
